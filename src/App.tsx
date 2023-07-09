a035536f97b01ab81a7d662f58830cf9960123a1
VDromaretska <viktoriya.dromaretska@gmail.com>
2023-07-09 23:27:33 +0200

    Sort alphabetically

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import { babyNames } from "./babyName";
 import AddNameButton from "./components/AddNameButton";
 import "./styles.css";
+import SearchBar from "./components/SearchBar";
 
 function App(): JSX.Element {
   return (
     <>
       <h1> Welcome to baby name picker!</h1>
       <hr />
+      <div>
+        <SearchBar />
+      </div>
       <div className="button-container">
-        {babyNames.map((b) => (
-          <AddNameButton baby={b} key={b.id} />
-        ))}
+        {babyNames
+          .sort((a, b) =>
+            a.name.localeCompare(b.name, undefined, { sensitivity: "base" })
+          )
+          .map((b) => (
+            <AddNameButton baby={b} key={b.id} />
+          ))}
       </div>
     </>
   );
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,4 @@
+function SearchBar(): JSX.Element {
+  return <input type="text" placeholder="Search for names.." />;
+}
+export default SearchBar;

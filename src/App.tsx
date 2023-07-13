aedebc31721717b55112b95c077ff498435c643d
VDromaretska <viktoriya.dromaretska@gmail.com>
2023-07-13 23:45:19 +0200

    Code that doesn't work

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { babyNames } from "./babyName";
-import AddNameButton from "./components/AddNameButton";
 import "./styles.css";
 import { useState } from "react";
 
+
 function App(): JSX.Element {
   const [text, setText] = useState("");
+  function buttonClass(b:BabyData): string{
+    return b.sex === "f" ? "girl-button" : "boy-button";}
+  const [clicked, setClicked] = useState(false);
+  let favouritesNames : BabyData[]=[];
+  
+  const handleClick = (b:BabyData) => {
+  setClicked(true);
+favouritesNames.push(b);
+}
 
   return (
     <>
@@ -19,13 +28,20 @@ function App(): JSX.Element {
         />
       </div>
       <div className="button-container">
-        {babyNames
-          .sort((a, b) =>
+        <hr />
+        {clicked ===true && 
+            favouritesNames?.map((b)=> (
+              <button className={buttonClass(b)} key={b.id}>{b.name}</button>
+            ))}
+      </div>
+      <div className="button-container">
+        {clicked ===false && 
+        babyNames.sort((a, b) =>
             a.name.localeCompare(b.name, undefined, { sensitivity: "base" })
           )
           .filter((n) => n.name.toLowerCase().includes(text.toLowerCase()))
           .map((b) => (
-            <AddNameButton baby={b} key={b.id} />
+            <button className={buttonClass(b)} onClick={()=>handleClick(b)} key={b.id}>{b.name}</button>
           ))}
       </div>
     </>
@@ -38,8 +54,4 @@ export type BabyData = {
   id: number;
   name: string;
   sex: string;
-};
-
-export type Baby = {
-  baby: BabyData;
 };
